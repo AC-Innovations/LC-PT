474aa57dcb1649a8c549d7f31c776fc51f8c665a
JontyWilson <wilson.jonty1@gmail.com>
2025-10-15 10:23:50 -0700

    feat: new header component

--- app/src/Components/Header/Header.jsx
+++ app/src/Components/Header/Header.jsx
@@ -1,7 +1,32 @@
+import "./Header.css";
+import logo from "../../assets/Photos/Logo.png";
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <section className="navbar">
+        <img src={logo} />
+
+        <div className="wrapper">
+          <ul className="menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Services">Services</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Header
+export default Header;
