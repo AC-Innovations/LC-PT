cfd4cb0fb883ba68af91388ebf7d48dcc549fc95
JontyWilson <wilson.jonty1@gmail.com>
2025-10-23 16:16:22 -0700

    feat: added Hours of Operation, added container for margins

--- app/src/Pages/Home/Home.jsx
+++ app/src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import "./Home.css";
 import Header from "../../Components/Header/Header.jsx";
 import Banner from "../../Components/Banner/Banner.jsx";
 import Footer from "../../Components/Footer/Footer.jsx";
+import Hero from "../../Components/Hero/Hero.jsx";
 
 import groupPhoto from "../../assets/Photos/groupPhoto.png";
 import serviceCardImage from "../../assets/Photos/serviceCardImage.jpg";
@@ -10,7 +11,14 @@ import sloganImage from "../../assets/Photos/sloganImage.jpg";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { useEffect } from "react";
+
 function Home() {
+  useEffect(() => {
+    //use this function to call other functions when the page loads (note: will execute twice because of StrictMode in main.jsx)
+    setDay();
+  });
+
   let slideButtonPress = (event) => {
     let quoteArray = document.getElementsByClassName("quote");
     let selectorArray = document.getElementsByClassName(
@@ -94,154 +102,275 @@ function Home() {
     }
   };
 
+  let setDay = () => {
+    let day = new Date().getDay();
+
+    let daysContainer = document.getElementById("days-container");
+    let weeklyHoursContainer = document.getElementById(
+      "weekly-hours-container"
+    );
+
+    for (let i = 0; i < daysContainer.children.length; i++) {
+      daysContainer.children[i].classList.remove("active");
+      weeklyHoursContainer.children[i].classList.remove("active");
+    }
+
+    //Note: Date().getDay() retrieves weekdays from 0-6
+    //If the container elements are changed or condensed, ensure there is no overflow
+    if (day <= daysContainer.children.length - 1) {
+      daysContainer.children[day].classList.add("active");
+      weeklyHoursContainer.children[day].classList.add("active");
+    }
+  };
+
   return (
     <>
-      <Header />
-      <section className="hero">
-        <img src={groupPhoto} alt="photo" />
-        <div id="heroOverlay"></div>
-
-        <div id="heroCTA">
-          <h1>Your Recovery, Our Passion</h1>
-          <p>Family owned and operated</p>
-          <button className="primaryButton">Book an Appointment</button>
-        </div>
-      </section>
-
-      <section className="services">
-        <h1>Our Services</h1>
-
-        <div className="wrapper">
-          <div className="services-card">
-            <img src={serviceCardImage} alt="physical therapy image"></img>
-            <div className="label-wrapper">
-              <p>Back & Neck Dysfunction</p>
-              <div className="accent-bar"></div>
-            </div>
-          </div>
-
-          <div className="services-card">
-            <img src={serviceCardImage} alt="physical therapy image"></img>
-            <div className="label-wrapper">
-              <p>Balance Training & Vestibular</p>
-              <div className="accent-bar"></div>
-            </div>
-          </div>
-
-          <div className="services-card">
-            <img src={serviceCardImage} alt="physical therapy image"></img>
-            <div className="label-wrapper">
-              <p>Orthopedic Injury</p>
-              <div className="accent-bar"></div>
-            </div>
-          </div>
-
-          <div className="services-card">
-            <img src={serviceCardImage} alt="physical therapy image"></img>
-            <div className="label-wrapper">
-              <p>Neuromuscular Training</p>
-              <div className="accent-bar"></div>
-            </div>
-          </div>
-        </div>
-
-        <button className="secondaryButton">View all services</button>
-      </section>
-
-      <section className="slogan">
-        <h1>We help you get back to doing what you love.</h1>
-        <img src={sloganImage}></img>
-      </section>
-
-      <section className="reviews">
-        <h2>What our patients have to say</h2>
-
-        <div className="carousel">
-          <div className="quote-wrapper">
-            <div className="quote">
-              <div className="stars">
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-              </div>
-              <p className="text">
-                Great crew working here. Very professional and knowledgeable.
-                They definitely care about their clients health and well being
-              </p>
-              <p className="author">- Nick O</p>
-            </div>
-            <div className="quote">
-              <div className="stars">
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-              </div>
-              <p className="text">
-                Makes therapy something to look forward to… the staff is
-                friendly and encouraging and I’m so happy I found them
-              </p>
-              <p className="author">- Deb Walter</p>
-            </div>
-
-            <div className="quote">
-              <div className="stars">
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-              </div>
-              <p className="text">
-                I've had total knee replacements on both knees. Came here for
-                therapy after each one. They know what they are doing and gave
-                me excellent care. Wonderful friendly staff. Highly recommend!
-              </p>
-              <p className="author">- Jeanne Prickett</p>
-            </div>
-            <div className="quote">
-              <div className="stars">
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-                <FontAwesomeIcon icon={faStar} color="gold" className="star" />
-              </div>
-              <p className="text">
-                Phenomenal staff, easy to work with, super flexible scheduling
-                too! Great place to go for PT! Highly recommended!
-              </p>
-              <p className="author">- Ryan Jones</p>
-            </div>
-          </div>
-          <div className="arrow-right" onClick={slideButtonPress}>
-            →
-          </div>
-          <div className="arrow-left" onClick={slideButtonPress}>
-            ←
-          </div>
-
-          <div className="slide-selector-container">
-            <div
-              className="slide-selector active"
-              onClick={slideButtonPress}
-            ></div>
-            <div className="slide-selector" onClick={slideButtonPress}></div>
-            <div className="slide-selector" onClick={slideButtonPress}></div>
-            <div className="slide-selector" onClick={slideButtonPress}></div>
-          </div>
-        </div>
-      </section>
-
-      <Banner
-        text={"Let us help you get back to doing what you love."}
-        btntext={"Contact Us"}
-        image={bannerimage}
-        imgalt={"instructor helping patient"}
+      <Hero
+        title={false}
+        image={groupPhoto}
+        enableCTA={true}
+        titleCTA={"Your Recovery, Our Passion"}
+        labelCTA={"Family owned and operated"}
+        buttonCTA={"Book an Appointment"}
       />
+      <div className="container">
+        <section className="services">
+          <h1>Our Services</h1>
+          <div className="wrapper">
+            <div className="services-card">
+              <img src={serviceCardImage} alt="physical therapy image"></img>
+              <div className="label-wrapper">
+                <p>Back & Neck Dysfunction</p>
+                <div className="accent-bar"></div>
+              </div>
+            </div>
+            <div className="services-card">
+              <img src={serviceCardImage} alt="physical therapy image"></img>
+              <div className="label-wrapper">
+                <p>Balance Training & Vestibular</p>
+                <div className="accent-bar"></div>
+              </div>
+            </div>
+            <div className="services-card">
+              <img src={serviceCardImage} alt="physical therapy image"></img>
+              <div className="label-wrapper">
+                <p>Orthopedic Injury</p>
+                <div className="accent-bar"></div>
+              </div>
+            </div>
+            <div className="services-card">
+              <img src={serviceCardImage} alt="physical therapy image"></img>
+              <div className="label-wrapper">
+                <p>Neuromuscular Training</p>
+                <div className="accent-bar"></div>
+              </div>
+            </div>
+          </div>
+          <button className="secondaryButton">View all services</button>
+        </section>
+        <section className="slogan">
+          <h1>We help you get back to doing what you love.</h1>
+          <img src={sloganImage}></img>
+        </section>
+
+        <section className="reviews">
+          <h2>What our patients have to say</h2>
+          <div className="carousel">
+            <div className="quote-wrapper">
+              <div className="quote">
+                <div className="stars">
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                </div>
+                <p className="text">
+                  Great crew working here. Very professional and knowledgeable.
+                  They definitely care about their clients health and well being
+                </p>
+                <p className="author">- Nick O</p>
+              </div>
+              <div className="quote">
+                <div className="stars">
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                </div>
+                <p className="text">
+                  Makes therapy something to look forward to… the staff is
+                  friendly and encouraging and I’m so happy I found them
+                </p>
+                <p className="author">- Deb Walter</p>
+              </div>
+              <div className="quote">
+                <div className="stars">
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                </div>
+                <p className="text">
+                  I've had total knee replacements on both knees. Came here for
+                  therapy after each one. They know what they are doing and gave
+                  me excellent care. Wonderful friendly staff. Highly recommend!
+                </p>
+                <p className="author">- Jeanne Prickett</p>
+              </div>
+              <div className="quote">
+                <div className="stars">
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    color="gold"
+                    className="star"
+                  />
+                </div>
+                <p className="text">
+                  Phenomenal staff, easy to work with, super flexible scheduling
+                  too! Great place to go for PT! Highly recommended!
+                </p>
+                <p className="author">- Ryan Jones</p>
+              </div>
+            </div>
+            <div className="arrow-right" onClick={slideButtonPress}>
+              →
+            </div>
+            <div className="arrow-left" onClick={slideButtonPress}>
+              ←
+            </div>
+            <div className="slide-selector-container">
+              <div
+                className="slide-selector active"
+                onClick={slideButtonPress}
+              ></div>
+              <div className="slide-selector" onClick={slideButtonPress}></div>
+              <div className="slide-selector" onClick={slideButtonPress}></div>
+              <div className="slide-selector" onClick={slideButtonPress}></div>
+            </div>
+          </div>
+        </section>
+
+        <section className="hours">
+          <div className="hours-box-container column">
+            <div className="hours-box">
+              <div id="days-container">
+                <p className="active">Sunday</p>
+                <p>Monday</p>
+                <p>Tuesday</p>
+                <p>Wednesday</p>
+                <p>Thursday</p>
+                <p>Friday</p>
+                <p>Saturday</p>
+              </div>
+              <div id="weekly-hours-container">
+                <p className="active">Closed</p>
+                <p>6 AM-5 PM</p>
+                <p>7 AM-5 PM</p>
+                <p>6:30 AM-5 PM</p>
+                <p>7 AM-5 PM</p>
+                <p>6 AM-12 PM</p>
+                <p>Closed</p>
+              </div>
+            </div>
+          </div>
+          <div className="accent-bar-container column">
+            <div className="hours-accent-bar"></div>
+          </div>
+          <h1 className="column">
+            Hours of <br></br> Operation
+          </h1>
+        </section>
+
+        <Banner
+          text={"A team you can trust. A difference you can feel."}
+          btntext={"Contact Us"}
+          image={bannerimage}
+          imgalt={"instructor helping patient"}
+        />
+      </div>
 
       <Footer />
     </>
