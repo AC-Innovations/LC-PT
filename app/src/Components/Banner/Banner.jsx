8810d75125339b82cff415e6e3af29a3a08577fe
JontyWilson <wilson.jonty1@gmail.com>
2025-10-24 17:47:37 -0700

    feat: about page, Banner update

--- app/src/Components/Banner/Banner.jsx
+++ app/src/Components/Banner/Banner.jsx
@@ -1,15 +1,22 @@
 import "./Banner.css";
 
-function Banner({ text, btntext, image, imgalt }) {
+function Banner({ text, btntext, image, imgalt, enableBackground, bgColor }) {
+  console.log(bgColor);
   return (
-    <div className="info-banner">
-      <div className="blue">
-        <label>{text}</label>
-        <button>{btntext}</button>
-      </div>
-
-      <div className="banner-img">
-        <img src={image} alt={imgalt} />
+    <div
+      className="banner-background"
+      style={{
+        backgroundColor: enableBackground ? `${bgColor}` : "#0000",
+      }}
+    >
+      <div className="info-banner">
+        <div className="blue">
+          <label>{text}</label>
+          <button>{btntext}</button>
+        </div>
+        <div className="banner-img">
+          <img src={image} alt={imgalt} />
+        </div>
       </div>
     </div>
   );
